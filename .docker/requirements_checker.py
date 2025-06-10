26cf0957bb04b248cfbc821fe0773e2e2f572c6f
Xalora <XaloraAI@outlook.com>
2025-06-10 11:28:46 -0400

    Update requirements_checker.py

--- .docker/requirements_checker.py
+++ .docker/requirements_checker.py
@@ -1,7 +1,7 @@
 import importlib
 import pkgutil
 import sys
-from pathlib import Path
+from pathlib import Path 
 
 project_root = Path(__file__).resolve().parent.parent
 sys.path.append(str(project_root))
